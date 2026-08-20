9e3182c32f15d406232a794caeed430b7b69ad77
Sloddervoss <268675390+Sloddervoss@users.noreply.github.com>
2026-08-20 19:26:33 +0200

    chore(simhub): bump version to 0.3.1.0

--- tools/simhub-plugin/3SM.EnduranceConnector/AssemblyInfo.cs
+++ tools/simhub-plugin/3SM.EnduranceConnector/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("3Stripe Motorsport")]
 [assembly: ComVisible(false)]
 [assembly: Guid("74d7436b-e986-4f2c-91c3-e5344f30ac33")]
-[assembly: AssemblyVersion("0.3.0.1")]
-[assembly: AssemblyFileVersion("0.3.0.1")]
+[assembly: AssemblyVersion("0.3.1.0")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
